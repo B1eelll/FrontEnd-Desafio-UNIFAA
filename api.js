3ec0cd8a5311bfcb5985f93672afdf2b90294546
Gabriel Alcântara <gabrielalcantaraproj@gmail.com>
2025-03-17 12:58:25 -0300

    Adição do CRUD (Post)
    Adição do crud, tela de carregamento.

--- api.js
+++ api.js
@@ -1,3 +1,57 @@
-let email = document.getElementById('email')
-let senha = document.getElementById('senha')
 
+// parte da API
+const login = document.getElementById('login')
+const btnentrar = document.getElementById('btnentrar')
+
+btnentrar.addEventListener('click', function(){
+const email = document.getElementById('email').value
+const senha = document.getElementById("senha").value
+
+
+fazerlogin(email, senha);
+})
+
+function fazerlogin(email, senha){ // função começa aqui 
+    fetch('http://localhost:3400/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            senha: senha
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('credenciais inválidas.');
+        }
+    })
+    .then(dados => {
+         loading.style.display = 'flex';
+              login.style.display = 'none'
+        setTimeout(function(){
+            window.location.href ='inicio.html';
+        } ,2000)
+    })
+         
+    .catch(erro => {
+        window.alert('Campos inválidos')
+    });
+} // função termina aqui
+
+//parte da interação com a página
+function entrar(){
+    btnentrar.style.backgroundColor = 'blue'
+}
+
+function sair(){
+    btnentrar.style.backgroundColor = ''
+}
+
+if(btnentrar){
+    btnentrar.addEventListener("mouseenter", entrar);
+    btnentrar.addEventListener("mouseleave", sair)
+}
