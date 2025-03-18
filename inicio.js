2541bf3d266d4b2cccaf819209bdaecad4c71fa5
Gabriel Alcântara <gabrielalcantaraproj@gmail.com>
2025-03-18 15:39:22 -0300

    Página hub

---
+++ inicio.js
@@ -0,0 +1,15 @@
+
+function obterclientes(){
+    fetch('http://localhost:3400/clientes', {
+        method: "GET",
+        headers: {
+            "authorization": "8ada0eccb3a9dc34c5316bfb97dccfbf"
+        }
+    })
+    .then(response => response.json())
+    .then(clientes => {
+        console.log(clientes);
+    })
+}
+
+obterclientes()
