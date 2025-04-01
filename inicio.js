604046c410e7c1784cfebb2d314a74fb2915ed5c
Gabriel Alcântara <gabrielalcantaraproj@gmail.com>
2025-03-31 22:47:57 -0300

    update (APARENTEMENTE, TUDO ESTABILIZADO!)

--- inicio.js
+++ inicio.js
@@ -1,13 +1,28 @@
-let clientes = document.getElementById('clientes') 
-clientes.addEventListener('click',function(){
+let Clientes = document.getElementById("clientes") 
+Clientes.addEventListener('click',function(){
     window.location.href = "clientes.html";
 })
 
-const logout = document.getElementById('logout').addEventListener('click', function(){
+const Logout = document.getElementById('logout').addEventListener('click', function(){
     localStorage.removeItem('token');
 })
-
-obtertoken()
+function salvarToken(token) {
+    localStorage.setItem("token", token); // Salva o token no local storage
+  }
+  
+  function salvarUsuario(usuario) {
+    localStorage.setItem("usuario", JSON.stringify(usuario)); // Salva o usuário no local storage
+  }
+  
+  function obterToken() {
+    return localStorage.getItem("token"); // Pega o token salvo no local storage
+  }
+  
+  function obterUsuario() {
+    return JSON.parse(localStorage.getItem("usuario")); // Pega o usuário salvo no local storage
+  }
+  salvarToken(token)
+  obterToken()
 
 
 /*function obterclientes(){
--- loginAPI.js
+++ loginAPI.js
@@ -30,6 +30,8 @@ function fazerlogin(email, senha) {
     })
     .then((dados) => {
       const token = dados.token;
+      salvarToken(token)
+      obterToken()
       loading.style.display = "flex";
       login.style.display = "none";
       setTimeout(function () {
@@ -41,7 +43,26 @@ function fazerlogin(email, senha) {
       window.alert("Erro ao fazer login");
     });
 } // função termina aqui
+const Logout = document.getElementById('logout').addEventListener('click', function(){
+  localStorage.removeItem('token');
+})
+function salvarToken(token) {
+  localStorage.setItem("token", token); // Salva o token no local storage
+}
 
+function salvarUsuario(usuario) {
+  localStorage.setItem("usuario", JSON.stringify(usuario)); // Salva o usuário no local storage
+}
+
+function obterToken() {
+  return localStorage.getItem("token"); // Pega o token salvo no local storage
+}
+
+function obterUsuario() {
+  return JSON.parse(localStorage.getItem("usuario")); // Pega o usuário salvo no local storage
+}
+salvarToken()
+obterToken()
 //parte da interação com a página
 function entrar() {
   btnentrar.style.backgroundColor = "blue";
@@ -55,4 +76,3 @@ if (btnentrar) {
   btnentrar.addEventListener("mouseenter", entrar);
   btnentrar.addEventListener("mouseleave", sair);
 }
-
